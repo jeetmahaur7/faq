1c18a23163b10c03ef7a3c807e24a966b2a2a4d1
Jeet Mahaur <132244263+jeetmahaur7@users.noreply.github.com>
2025-06-10 15:10:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,9 @@
-// import "./App.css";
-// import Faq from "./components/faq/Faq";
-
-// function App() {
-//   return (
-//     <>
-//       <Faq />
-//     </>
-//   );
-// }
-// export default App;
-
-// import "./App.css";
-// import Counter from "./components/counter/Counter";
-
-// function App() {
-//   return (
-//     <>
-//       <Counter />
-//     </>
-//   );
-// }
-// export default App;
-
 import "./App.css";
-import Calculator from "./components/calculator/Calculator";
+import Faq from "./components/faq/Faq";
 function App() {
   return (
     <>
-      <Calculator />
+      <Faq />
     </>
   );
 }
